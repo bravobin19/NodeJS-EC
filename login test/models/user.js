7f6142ad04e91f089ebc15dfb824a2756ec09b91
Hiếu Trịnh <78504039+bravobin19@users.noreply.github.com>
2022-08-25 18:00:01 +0700

    hello

---
+++ login test/models/user.js
@@ -0,0 +1,34 @@
+// 1
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+
+
+
+
+
+// 2
+const userSchema = new Schema({
+  email: String,
+  username: String,
+  password: String
+}, {
+ 
+  // 3
+  timestamps: {
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt'
+  }
+})
+ 
+// 4
+const User = mongoose.model('user', userSchema);
+User.hashPassword = async (password) => {
+    try {
+      const salt = await bcrypt.genSalt(10)
+      return await bcrypt.hash(password, salt)
+    } catch(error) {
+      throw new Error('Hashing failed', error)
+    }
+};
+module.exports = User;
